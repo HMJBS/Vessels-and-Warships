c7773330e567c4eaa08b5c5efad25d8126000a27
Yohei <Yohei@Yohei-PC>
2017-03-21 16:37:47 +0900

    add BlockChrysanthemum.class

---
+++ src/main/java/com/vineLadder/shipsandvessels/block/BlockChrysanthemum.java
@@ -0,0 +1,13 @@
+package com.vineLadder.shipsandvessels.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockChrysanthemum extends Block {
+
+	protected BlockChrysanthemum(String unlocalizedName,Material material) {
+		super(material);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+}
